8674de7931811c1f5818adc2787abbb7077a8209
yuchin <yuchinpao@liontravel.com>
2019-07-15 23:24:14 +0800

    Add docs

---
+++ docs/precache-manifest.10c543e0d3df7962f110ff59d819e38c.js
@@ -0,0 +1,19 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "94633791913b18b7f5d74203ce29ef5a",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.872c9dbf8b8eef215877.bundle.js"
+  },
+  {
+    "revision": "22d2d655fb69114f31d1",
+    "url": "main.css"
+  },
+  {
+    "url": "runtime~main.872c9dbf8b8eef215877.bundle.js"
+  },
+  {
+    "url": "vendors~main.872c9dbf8b8eef215877.bundle.js"
+  }
+]);
